1dd615cbaaf2d7209c26fbdac7088673f07b884f
calvin <calvin.francis96@outlook.com>
2021-09-10 17:35:49 -0400

    Update controller actions for Rating resource

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,11 +2,14 @@ class RatingsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
     def create
-
+        rating = Rating.create(rating_params)
+        render json: rating, status: :created
     end
 
     def update
-
+        rating = find_rating
+        rating.update(rating_params)
+        render json: rating
     end
 
     private
