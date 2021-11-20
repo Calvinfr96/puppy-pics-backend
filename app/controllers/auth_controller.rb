3a0d8ee26ac9b344c748e460f5cdfb70520497ec
calvin <calvin.francis96@outlook.com>
2021-11-20 11:28:37 -0600

    Configure Auth Controller to issue token to user when successfully logged in and check current user token on each request

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,24 @@
+class AuthController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
+    def create
+        @user = User.find_by(name: auth_params[:name])
+        if @user && @user.authenticate(auth_params[:password])
+            @token = encode_token({user_id: @user.id})
+            render json: {user: UserSerializer.new(@user), jwt: @token}, status: :accepted
+        else
+            rener json: {error: 'Invalid Username or Password'}, status: :unauthorized
+        end
+    end
+
+    def check_user
+        @user = current_user
+        render json: {user: UserSerializer.new(@user)}, status: :accepted
+    end
+
+    private
+
+    def auth_params
+        params.require(:authorization).permit(:name, :password)
+    end
+end
