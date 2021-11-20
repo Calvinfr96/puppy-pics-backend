9cec0efb4274c091258fb2d35d41dbe2628c44be
calvin <calvin.francis96@outlook.com>
2021-11-20 12:39:02 -0600

    Add routes for login and authenticating user on each request

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :dogs, only: [:index, :create, :destroy]
   resources :breeds, only: [:index, :show, :create]
   resources :ratings, only: [:index, :create, :update]
+  post '/login', to: 'auth#create'
+  get '/me', to: 'auth#check_user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
